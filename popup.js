560ca0d1e9496ce7f124aa71c4d5b68df49a2e0e
jayachankura <jayachankura@gmail.com>
2023-04-21 14:30:49 -0400

    add package lock json

--- popup.js
+++ popup.js
@@ -1,4 +1,12 @@
-alert("working");
 const button = document.querySelector("button");
-const testFunc = () => console.log("click");
+const body = document.querySelector("body");
+
+// test putting cat image on website
+const img = document.createElement("img");
+img.setAttribute(
+  "src",
+  "https://images.rawpixel.com/image_png_social_square/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA5L3B4NzU1MDcwLWltYWdlLWpvYjEzMTUucG5n.png"
+);
+
+const testFunc = () => body.appendChild(img);
 button.onclick = () => testFunc();
